53115076d5ad1ca9d9d9e3bd5414ad15a336acd3
Svitlana-G <svitlana.golubieva@gmail.com>
2026-01-14 16:59:23 +0100

    Add styles for how it all began section

--- src/components/HowItAllBeganSection/HowItAllBeganSection.tsx
+++ src/components/HowItAllBeganSection/HowItAllBeganSection.tsx
@@ -1,27 +1,32 @@
 import handshaking from '../../assets/images//HowItAllBegan/handshaking.webp';
+import './HowItAllBeganSection.css';
 
 const HowItAllBeganSection = () => {
   return (
-    <section className="how-it-all-began-section">
-      <h2>Як все почалося</h2>
-      <p>
-        У липні 2025 року ми об‘єдналися, щоб використати професійні навички для
-        соціального проєкту. Віримо, що платформа Adoptly зробить прилаштування
-        тварин простим, прозорим та доступним.
-      </p>
-      <div>
+    <section className="how-it-all-began-section container">
+      <div className="first-block">
+        <h3>Як все почалося?</h3>
+        <p>
+          У липні 2025 року ми об‘єдналися, щоб використати професійні навички
+          для соціального проєкту. Віримо, що платформа Adoptly зробить
+          прилаштування тварин простим, прозорим та доступним.
+        </p>
+      </div>
+      <div className="second-block">
         <img
           src={handshaking}
           alt="handshaking"
           loading="lazy"
           decoding="async"
         />
-        <h3>Народжені на Team Challenge </h3>
-        <p>
-          Основна команда сформувалася на платформі Team Challenge – де
-          інженери, дизайнери, QA та менеджери знаходять партнерів для перших
-          значущих проєктів.
-        </p>
+        <div>
+          <h4>Народжені на Team Challenge </h4>
+          <p>
+            Основна команда сформувалася на платформі Team Challenge – де
+            інженери, дизайнери, QA та менеджери знаходять партнерів для перших
+            значущих проєктів.
+          </p>
+        </div>
       </div>
     </section>
   );
