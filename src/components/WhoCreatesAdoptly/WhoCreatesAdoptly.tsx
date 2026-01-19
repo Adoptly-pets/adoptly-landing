678c9de3fe9170bb6769910c8f5b76d70db790a1
Svitlana-G <svitlana.golubieva@gmail.com>
2026-01-19 15:40:34 +0100

    Add section WhoCreatesAdoptly

---
+++ src/components/WhoCreatesAdoptly/WhoCreatesAdoptly.tsx
@@ -0,0 +1,12 @@
+import './WhoCreatesAdoptly.css';
+
+const WhoCreatesAdoptly = () => {
+  return (
+    <div className="who-creates-Adoptly">
+      <h2>Хто створює Adoptly?</h2>
+      <p className="subtitle">Команда волонтерів, обʼєднаних спільною метою.</p>
+    </div>
+  );
+};
+
+export default WhoCreatesAdoptly;
