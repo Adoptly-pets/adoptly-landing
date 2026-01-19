cfcbb34a6616cfe9a7224824683937f471e9fed4
Svitlana-G <svitlana.golubieva@gmail.com>
2026-01-19 16:36:28 +0100

    Add styles for Roles component

--- src/components/Roles/Roles.tsx
+++ src/components/Roles/Roles.tsx
@@ -1,10 +1,11 @@
 import { ROLES_DATA } from '../../constants/ROLES_DATA';
+import './Roles.css';
 
 const Roles = () => {
   return (
     <div className="roles">
       {ROLES_DATA.map(role => (
-        <div key={role.name} className="role">
+        <div key={role.name} className="role-card">
           <img src={role.icon} alt={role.name} />
           <p>{role.name}</p>
         </div>
