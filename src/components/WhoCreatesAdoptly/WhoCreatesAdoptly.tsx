b2ac393b5d3a21fa917b2a49bd3ce9bc1d4cbc89
Svitlana-G <svitlana.golubieva@gmail.com>
2026-01-20 17:54:59 +0100

    Add paws

--- src/components/WhoCreatesAdoptly/WhoCreatesAdoptly.tsx
+++ src/components/WhoCreatesAdoptly/WhoCreatesAdoptly.tsx
@@ -1,10 +1,19 @@
 import Roles from '../Roles/Roles';
 import './WhoCreatesAdoptly.css';
 import Button from '../Button/Button';
+import footprints from '../../assets/images/WhoCreatesAdoptly/footprints.webp';
 
 const WhoCreatesAdoptly = () => {
   return (
-    <section className="who-creates-Adoptly">
+    <section className="who-creates-adoptly">
+      <img
+        className="footprints paws-1"
+        src={footprints}
+        alt="footprints"
+        aria-hidden="true"
+        loading="lazy"
+        decoding="async"
+      />
       <h2>Хто створює Adoptly?</h2>
       <p className="subtitle">Команда волонтерів, обʼєднаних спільною метою.</p>
       <Roles />
@@ -19,6 +28,14 @@ const WhoCreatesAdoptly = () => {
       >
         Долучитися до команди
       </Button>
+      <img
+        className="footprints paws-2"
+        src={footprints}
+        alt="footprint"
+        aria-hidden="true"
+        loading="lazy"
+        decoding="async"
+      />
     </section>
   );
 };
