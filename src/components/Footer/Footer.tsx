3e7eed03a01176da4155f603e4c0ee090e8e709d
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2026-02-17 13:20:03 +0100

    fix: header and footer, changed mobile layout width

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,14 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-content">
-        <div className="footer-logo">
-          <Icon id="icon-footprint" size={24} height={24} />
-          <span>Adoptly 2026</span>{' '}
+      <div className="footer">
+        <div className="footer-content">
+          <div className="footer-logo">
+            <Icon id="icon-footprint" size={24} height={24} />
+            <span>Adoptly 2026</span>{' '}
+          </div>
+          <p>Зроблено з любов’ю до тварин</p>
         </div>
-        <p>Зроблено з любов’ю до тварин</p>
       </div>
     </footer>
   );
